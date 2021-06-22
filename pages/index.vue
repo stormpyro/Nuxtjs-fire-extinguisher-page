b593f41be12c2b043e894786ab0c578cc38aaebf
Stormpyro <tatto1795@gmail.com>
2021-06-22 10:46:50 -0500

    CLIENT - Add cards components

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections } from "@/assets/data.js";
+export default {
+  data: () => ({
+    largeCardInfo: largeCardSections,
+  }),
+};
+</script>
+
+<style>
+</style>
